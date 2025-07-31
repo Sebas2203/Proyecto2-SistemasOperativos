f009fc0d05f9a760512183d92ebed2c964cf53a3
Nicole <Nicole@DESKTOP-I6824JN>
2025-07-31 13:16:21 -0600

    REQ-InterFRMMantenimientoViajes

---
+++ Presentacion/FrmMantenimientoViajes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Servidor.Presentacion
+{
+    public partial class FrmMantenimientoViajes : Form
+    {
+        public FrmMantenimientoViajes()
+        {
+            InitializeComponent();
+        }
+    }
+}
