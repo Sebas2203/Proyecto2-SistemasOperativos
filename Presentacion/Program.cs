3e4cc8c4a7c8481a51e11d2aa34996726c26a997
Sebastian Mora <133081239+Sebas2203@users.noreply.github.com>
2025-08-14 21:13:20 -0600

    Servidor funcional, no funciona la cantidad de asientos ahora hora de mostrarlo en el cliente

--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -9,24 +9,27 @@ namespace Servidor
 {
     internal static class Program
     {
-
+        [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Principal());
-            /*Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            // Abrir primero el mantenimiento de viajes
-            using (FrmMantenimientoViajes frmMantenimiento = new FrmMantenimientoViajes())
-            {
-                frmMantenimiento.ShowDialog();
-            }
-
-            // Al cerrarlo, abrir el formulario Principal
-            Application.Run(new Principal());*/
             Application.Run(new MenuPrincipall());
+            //Application.Run(new Principal());
+            //Application.Run(new FrmMantenimientoViajes());
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+
+            ////Abrir primero el mantenimiento de viajes
+            //using (FrmMantenimientoViajes frmMantenimiento = new FrmMantenimientoViajes())
+            //{
+            //    frmMantenimiento.ShowDialog();
+            //}
+
+            ////Al cerrarlo, abrir el formulario Principal
+            //Application.Run(new Principal());
+            ////Application.Run(new MenuPrincipall());
         }
     }
 }
